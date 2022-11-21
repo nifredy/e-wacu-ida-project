0df530b21e09d00037c6d3d6efab1d59a4489fcf
nifredy <fredydominic0@yahoo.com>
2022-11-21 23:41:51 +0200

    team section

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@ const selectElement = (element) => document.querySelector(element);
 selectElement(element='.menu-icons').addEventListener('click', () => {
     selectElement(element='nav').classList.toggle(token='active');
 });
+
+//scroll uppppppppp staffff
+
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+  
+    if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+  }
+  window.addEventListener("scroll", scrollUp);
+  
+  // background header sticky
+  
+  function scrollHeader() {
+    const nav = document.getElementById("header");
+  
+    if (this.scrollY >= 5) nav.classList.add("sticky");
+    else nav.classList.remove("sticky");
+  }
+  window.addEventListener("scroll", scrollHeader);
